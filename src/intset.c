7350e6a9ab213d368ec1472c478954fa703b0a1f
weekend27 <weekend27@163.com>
2016-03-31 11:23:22 +0800

    intset

---
+++ src/intset.c
@@ -0,0 +1,43 @@
+#include "intset.h"
+#include <stdio.h>
+#include <string.h>
+
+#include "endian.h"
+
+uint8_t intset_get(intset *is, int pos, int64_t *v) {
+    int64_t v64;
+    int32_t v32;
+    int16_t v16;
+
+    if (pos >= is->length) return 0;
+
+    if (is->encoding == sizeof(int64_t)) {
+        memcpy(&v64, (int64_t*)is->contents + pos, sizeof(int64_t));
+        memrev64ifbe(&v64);
+        *v = v64;
+    } else if (is->encoding == sizeof(int32_t)) {
+        memcpy(&v32, (int32_t*)is->contents + pos, sizeof(int32_t));
+        memrev32ifbe(&v32);
+        *v = v32;
+    } else {
+        memcpy(&v16, (int16_t*)is->contents + pos, sizeof(int16_t));
+        memrev16ifbe(&v16);
+        *v = v16;
+    }
+
+    return 1;
+}
+
+void intset_dump(intset *is) {
+    printf("encoding: %d\n", (int)is->encoding);
+    printf("length: %d\n", (int)is->length);
+
+    int i;
+    int64_t v;
+    printf("element {\t");
+    for (i = 0; i < is->length; ++i) {
+        intset_get(is, i, &v);
+        printf("%lld\t", v);
+    }
+    printf("}\n");
+}
---
+++ src/intset.h
@@ -0,0 +1,13 @@
+#ifndef _INTSET_H_
+#define _INTSET_H_
+
+#include <stdint.h>
+typedef struct {
+    uint32_t encoding;  /* int16, int32, int64 */
+    uint32_t length;
+    uint8_t contents[0];
+} intset;
+
+void intset_dump(intset *is);
+uint8_t intset_get(intset *is, int pos, int64_t *v);
+#endif
